32281ab57d5add07414b892384333c6a54d7e389
KeeganBruer <keegan.bruer@gmail.com>
2017-05-28 23:19:35 -0400

    Updated Global Variables class and README file

--- src/com/team5854/robot/GlobalVariables.java
+++ src/com/team5854/robot/GlobalVariables.java
@@ -3,7 +3,12 @@ package com.team5854.robot;
 import com.team5854.robot.utils.RobotState;
 import com.team5854.robot.utils.TalonDrive;
 
+import edu.wpi.first.wpilibj.Joystick;
+
 public class GlobalVariables {
 	public static RobotState RoboState; 
+	public static Joystick js0 = new Joystick(0);
+	public static Joystick js1 = new Joystick(1);
 	public static TalonDrive drive = new TalonDrive(true);
+
 }
